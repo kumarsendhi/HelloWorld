2969d85bbf518f84caa28a42e7909782a29b9083
unknown <krishk6@INCSKRISHK6L5C.corp.emc.com>
2015-12-22 22:53:14 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+
+app.get('/',function(req,res){
+res.json({message:'hooray! welcome to our api!'});
+});
+
+
+app.listen(port);
